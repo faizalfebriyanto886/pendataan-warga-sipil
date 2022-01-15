ec066714b7f525fc8953ad0af819a8c2c5670e4e
faizalfebriyanto886 <faizalfebriyanto886@gmail.com>
2022-01-15 14:57:46 +0700

    new desain home

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -38,101 +38,136 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 60),
-            Container(
-              padding: const EdgeInsets.only(top: 10),
-              child: Row(
-                children: [
-                  const Text(
-                    "Dashboard",
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 80),
+            decoration: BoxDecoration(
+              color: primary,
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.elliptical(
+                  MediaQuery.of(context).size.width, 100.0
+                )
+              ),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 80, 10, 10),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "Dashboard",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      const SizedBox(width: 210),
+                      ClipRRect(
+                        borderRadius: const BorderRadius.all(Radius.circular(10)),
+                        child: Image.asset('assets/images/profile.png', width: 40, height: 40,),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const SayHello()
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    "Penduduk",
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold
                     ),
                   ),
-                  const SizedBox(width: 200),
-                  ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    child: Image.asset(
-                      'assets/images/profile.png',
-                      width: 40,
-                      height: 40,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Card(
-              color: primary,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                    child: Row(
-                      children: const [
-                        SizedBox(width: 10),
-                        Text(
-                          "Hai Sonep",
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white
-                          ),
-                        ),
-                        SizedBox(width: 60),
-                        Text(
-                          "Hai sonep",
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white
-                          ),
-                        ),
-                        SizedBox(width: 60),
-                        Text(
-                          "Hai sonep",
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              children: const [
-                SizedBox(height: 10, width: 5,),
-                Text(
-                  "Pengeluaran harian",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                  ),
                 ),
-                SizedBox(width: 180,),
-                TextButton(
-                  onPressed: null,
-                  child: Text("See all", style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
+                const SizedBox(height: 10),
+                Row(
+                  children: const [
+                    Penduduk(),
+                    Penduduk(),
+                    Penduduk(),
+                  ],
                 )
               ],
             ),
-            Row(
-              children: [
-                
-              ],
-            )
-          ],
-        ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class SayHello extends StatelessWidget {
+  const SayHello({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: const [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Hello Nep",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Colors.white
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            "Proin eget tortor risus. Cras ultricies ligula sed magna dictum porta.",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: Colors.white
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Penduduk extends StatelessWidget {
+  const Penduduk({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Image.asset('assets/images/sehat.png', height: 100, width: 100,),
+              const SizedBox(height: 5),
+              const Text(
+                "Warga Sehat",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/colors.dart
+++ lib/widgets/colors.dart
@@ -1,3 +1,3 @@
 import 'package:flutter/cupertino.dart';
 
-Color primary = const Color(0xFFFF7C51);
+Color primary = const Color(0xFF473F97);
